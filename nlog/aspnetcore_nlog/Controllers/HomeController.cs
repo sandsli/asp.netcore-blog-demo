d11681baee299948eccaab48bd283331f416ae7a
sandsli <543793753@qq.com>
2019-08-02 10:53:40 +0800

    a

---
+++ nlog/aspnetcore_nlog/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aspnetcore_nlog.Models;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetcore_nlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        //定义logger接口
+        private static ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("info test");
+            _logger.LogTrace("trace test");
+            _logger.LogError("error test");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ nlog/aspnetcore_nlog/Controllers/NLogController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aspnetcore_nlog.Models;
+
+namespace aspnetcore_nlog.Controllers
+{
+    public class NLogController : Controller
+    {
+        //定义logger接口
+        private static NLog.Logger _logger;
+
+        public NLogController()
+        {
+            _logger = NLog.LogManager.GetCurrentClassLogger();
+        }
+
+        public IActionResult Index()
+        {
+            _logger.Error("nlog test");
+            return Content("nlog");
+        }
+
+     
+    }
+}
