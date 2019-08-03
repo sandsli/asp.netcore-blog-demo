998cb96bd47f056f938db89842b6b94c5e815651
sandsli <543793753@qq.com>
2019-08-03 14:05:44 +0800

    a

--- ReflectionSet/ConsoleApplication1/FFoo.cs
+++ ReflectionSet/ConsoleApplication1/FFoo.cs
@@ -23,27 +23,23 @@ namespace ConsoleApplication1
         public static void SetReflection(Foo foo, Bar bar)
         {
             var property = foo.GetType().GetProperty("Bar");
-
             property.SetValue(foo, bar);
         }
 
         public static Action<Foo, Bar> SetExpression()
         {
             var property = typeof(Foo).GetProperty("Bar");
-
             var target = Expression.Parameter(typeof(Foo));
             ParameterExpression propertyValue = Expression.Parameter(typeof(Bar));
             //var setPropertyValue = Expression.Call(target, property.GetSetMethod(), propertyValue);
             BinaryExpression setPropertyValue = Expression.Assign(Expression.Property(target, property), propertyValue);
             var setAction = Expression.Lambda<Action<Foo, Bar>>(setPropertyValue, target, propertyValue).Compile();
-            //setAction(foo, bar);
             return setAction;
         }
 
         public static Action<Foo, Bar> SetEmit()
         {
             var property = typeof(Foo).GetProperty("Bar");
-
             DynamicMethod method = new DynamicMethod("SetValue", null, new Type[] { typeof(Foo), typeof(Bar) });
             ILGenerator ilGenerator = method.GetILGenerator();
             ilGenerator.Emit(OpCodes.Ldarg_0);
@@ -53,57 +49,45 @@ namespace ConsoleApplication1
             method.DefineParameter(1, ParameterAttributes.In, "obj");
             method.DefineParameter(2, ParameterAttributes.In, "value");
             var setAction2 = (Action<Foo, Bar>)method.CreateDelegate(typeof(Action<Foo, Bar>));
-            //setAction2(foo, bar);
             return setAction2;
-
         }
-
 
         public static void Test()
         {
+            var act1 = Foo.SetExpression();
+            var act2 = Foo.SetEmit();
 
-            var st0 = new Stopwatch();
-            st0.Start();
+            var st = new Stopwatch();
+            st.Start();
 
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 1000000; i++)
             {
                 var foo = new Foo { };
                 var bar = new Bar { Id = 1, Name = "name" };
                 Foo.SetReflection(foo, bar);
             }
 
-            st0.Stop();
-            Console.WriteLine("Reflection " + st0.ElapsedMilliseconds);
+            Console.WriteLine("Reflection " + st.ElapsedMilliseconds);
 
-            var st1 = new Stopwatch();
-            st1.Start();
-
-            var act1 = Foo.SetExpression();
-            for (int i = 0; i < 100000; i++)
+            st.Restart();
+            for (int i = 0; i < 1000000; i++)
             {
                 var foo = new Foo { };
                 var bar = new Bar { Id = 1, Name = "name" };
                 act1(foo, bar);
             }
 
-            st1.Stop();
-            Console.WriteLine("Expression " + st1.ElapsedMilliseconds);
+            Console.WriteLine("Expression " + st.ElapsedMilliseconds);
 
-            var st2 = new Stopwatch();
-            st2.Start();
-
-            var act2 = Foo.SetEmit();
-            for (int i = 0; i < 100000; i++)
+            st.Restart();
+            for (int i = 0; i < 1000000; i++)
             {
                 var foo = new Foo { };
                 var bar = new Bar { Id = 1, Name = "name" };
                 act2(foo, bar);
             }
 
-            st2.Stop();
-            Console.WriteLine("Emit " + st2.ElapsedMilliseconds);
+            Console.WriteLine("Emit " + st.ElapsedMilliseconds);
         }
     }
-
-
 }
--- ReflectionSet/ConsoleApplication1/Program.cs
+++ ReflectionSet/ConsoleApplication1/Program.cs
@@ -11,26 +11,13 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            FastReflection.Program.Maintest();
+            //FastReflection.Program.Maintest();
 
-            //Foo.Test();
+            Foo.Test();
             //People.Test();
             Console.Read();
         }
 
-        //private static void MyGet(Foo foo, System.Reflection.PropertyInfo p)
-        //{
-
-        //   // var result = p.GetValue(foo);
-
-        //}
-
-        //private static void MyGet2(Foo foo, System.Reflection.PropertyInfo p)
-        //{
-        //    //var target = Expression.Parameter(typeof(Foo));
-        //    //var getPropertyValue = Expression.Property(target, "Name");
-        //    //var getfunc = Expression.Lambda<Func<Foo, string>>(getPropertyValue, target).Compile();
-        //    //var result = getfunc(foo);
-        //}
+        
     }
 }
